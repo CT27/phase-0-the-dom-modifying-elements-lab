17ad8d63f8b3d9b8d2b39392dec30c807caa015d
Candice Thomason <candice.thomason@gmail.com>
2024-06-28 10:33:09 +1000

    3 out of 4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+const message = document.getElementById("victory");
+message.textContent = "Candice is the champion";
+
+document.body.appendChild(newHeader);
